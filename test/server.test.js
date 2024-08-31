e30a7dbae92563fd105b5590e3ede2d435b357c2
Striver-3110 <21csjay067@ldce.ac.in>
2024-08-31 09:36:24 +0530

    failing test case for non existent route

--- test/server.test.js
+++ test/server.test.js
@@ -3,20 +3,26 @@ const express = require('express');
 const { describe } = require('node:test');
 const { request } = require('http');
 
+const APP = 'http://localhost:5001';
+
 describe('server is running properly',()=>{
-    let app;
-
-    beforeAll(()=>{
-        // Initialize the express app
-        app = express();
-    })
     
-    // 
+    // let app;
+    // beforeAll(()=>{
+    //     // Initialize the express app
+    //     app = express();
+    // })
+    
+    // should respond to the GET request at the root url
     test('should respond to the GET request at the root url',async()=>{
-        const response = await supertest('http://localhost:5001').get('/');
-
-        // failing test case
+        const response = await supertest(APP).get('/');
+        // failing test case passed
         expect(response.statusCode).toBe(200);
     })
 })
 
+
+test('should return 404 for a non-existent route', async () => {
+    const response = await supertest(APP).get('/non-existent-route');
+    expect(response.statusCode).toBe(200);
+});
