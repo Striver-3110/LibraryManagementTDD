8d94607514c6384cd285f446bb1e5ee395c02def
Striver-3110 <21csjay067@ldce.ac.in>
2024-08-31 13:28:52 +0530

    should handle get request on /viewAvailableBooks; failing test case

---
+++ test/book/viewAvailableBooks.test.js
@@ -0,0 +1,13 @@
+const supertest = require('supertest')
+require('dotenv').config();
+
+describe('View available book API',()=>{
+    // returns all the available books
+    test('should return all the available books', async () =>{
+        const response = await supertest(process.env.APP).get('/api/v1/Book/returnAvailableBooks')
+        expect(response.statusCode).toBe(200)
+        expect(response.body.success).toBe(true)
+        expect(response.body.message).toBe('All the available books returned successfully')
+        expect(response.body).toHaveProperty('availableBooks')
+    })
+})
